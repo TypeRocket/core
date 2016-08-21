8d8f60f065c2a09cdddc83c84a348f531c1a19f2
kevindees <kevin@robojuice.com>
2016-08-20 21:58:16 -0400

    wip exclude wilds

--- src/Http/Middleware/BaseVerify.php
+++ src/Http/Middleware/BaseVerify.php
@@ -17,16 +17,8 @@ class BaseVerify extends Middleware  {
     public function handle() {
 
         $path = $this->request->getPath();
-        $process = true;
 
-        foreach ($this->except as $exclude ) {
-            if( trim($exclude, '/') === trim($path, '/') ) {
-                $process = false;
-                break;
-            }
-        };
-
-        if( $process ) {
+        if( ! $this->excludePath($path) ) {
             if( $this->request->getMethod() != 'GET' ) {
                 $token = check_ajax_referer( 'form_' . Config::getSeed(), '_tr_nonce_form', false );
                 if ( ! $token ) {
@@ -40,5 +32,29 @@ class BaseVerify extends Middleware  {
 
 
         $this->next->handle();
+    }
+
+    public function excludePath($path)
+    {
+        $path = trim($path, '/');
+        foreach ($this->except as $exclude ) {
+            $exclude = explode( '/', trim($exclude, '/') );
+            $path = explode('/', $path);
+            $excluding = true;
+
+            foreach ( $path as $index => $part ) {
+                if( $exclude[$index] != '*' && $exclude[$index] != $part ) {
+                    $excluding = false;
+                    break;
+                }
+            }
+
+            if($excluding) {
+                return true;
+            }
+
+        };
+
+        return false;
     }
 }
