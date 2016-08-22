ca326059f973bcd24ee72a84394c5726741a9d19
kevin dees <kevin@robojuice.com>
2016-08-22 19:05:15 -0400

    add model casting to results

--- src/Database/Results.php
+++ src/Database/Results.php
@@ -3,6 +3,9 @@ namespace TypeRocket\Database;
 
 class Results extends \ArrayObject
 {
+
+    public $modelClass = null;
+
     /**
      * Add item to top of collection
      *
@@ -13,5 +16,26 @@ class Results extends \ArrayObject
         $array = $this->getArrayCopy();
         array_unshift( $array, $value );
         $this->exchangeArray( $array );
+    }
+
+    /**
+     * Cast results to Model
+     */
+    public function castResultsToModel()
+    {
+        if( ! $this->modelClass) {
+            return;
+        }
+
+        $array = $this->getArrayCopy();
+        $models = [];
+        if(!empty($array)) {
+            foreach ( $array as $item ) {
+                $model = (new $this->modelClass);
+                $model->properties = (array) $item;
+                $models[] = $model;
+            }
+        }
+        $this->exchangeArray( $models );
     }
 }
