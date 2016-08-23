57940b7156ef5788c542be584c41812e7e5b3c80
kevin dees <kevin@robojuice.com>
2016-08-23 10:10:19 -0400

    wip ci

--- bin/travis.sh
+++ bin/travis.sh
@@ -25,5 +25,5 @@ echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$PHPVERSION/etc/php.ini
 
 # configure apache virtual hosts
 sudo cp -f bin/travis-ci-apache /etc/apache2/sites-available/default
-sudo sed -e "s/%TRAVIS_BUILD_DIR%/$(pwd)/g" --in-place /etc/apache2/sites-available/default
+sudo sed -i "s/%TRAVIS_BUILD_DIR%/$(pwd)/g" /etc/apache2/sites-available/default
 sudo service apache2 restart
