6ae1b2b8e24e3f8e6239a422c523727e8cee8916
kevin dees <kevin@robojuice.com>
2016-08-23 15:43:18 -0400

    update query select test

--- tests/Query/SelectTest.php
+++ tests/Query/SelectTest.php
@@ -2,20 +2,26 @@
 class SelectTest extends PHPUnit_Framework_TestCase
 {
 
-    public function testSimpleSelect()
+    public function testSelectWithUppercase()
     {
         require BASE_WP;
 
         $query = new \TypeRocket\Database\Query();
         $query->table = 'wp_posts';
         $query->idColumn = 'ID';
+        $query->select('post_title', 'ID')->where('ID', 1)->get();
+        $this->assertTrue( $query->lastCompiledSQL == "SELECT `post_title`,`ID` FROM wp_posts WHERE ID = '1'" );
+    }
 
+    public function testSelectReturnsResults()
+    {
+        require BASE_WP;
+
+        $query = new \TypeRocket\Database\Query();
+        $query->table = 'wp_posts';
+        $query->idColumn = 'ID';
         $result = $query->select('post_title', 'ID')->where('ID', 1)->get();
-
-        var_dump($query->lastCompiledSQL);
-        var_dump($result);
-
-        $this->assertTrue( $query->lastCompiledSQL == 'SELECT `post_title`, `ID` FROM wp_posts WHERE ID = \'1\'' );
+        $this->assertInstanceOf( \TypeRocket\Database\Results::class , $result );
     }
 
 }
