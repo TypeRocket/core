678b3f9acfaac81a1104b05a99fd1788b9d994f1
kevin dees <kevin@robojuice.com>
2016-08-23 10:46:19 -0400

    wip ci

--- tests/Sanitize/SanitizeTest.php
+++ tests/Sanitize/SanitizeTest.php
@@ -2,14 +2,21 @@
 
 class SanitizeTest extends PHPUnit_Framework_TestCase
 {
-    public function testEmailFieldPasses()
+    public function testSanitizeDash()
     {
         require BASE_WP;
 
-        $string = 'exam!ple@';
+        $string = 'exam!ple_-@';
         $sanitized = \TypeRocket\Utility\Sanitize::dash($string);
-        var_dump($sanitized, $string);
+        $this->assertTrue( $sanitized == 'example--' );
+    }
 
-        $this->assertTrue( $sanitized == 'example' );
+    public function testSanitizeUnderscore()
+    {
+        require BASE_WP;
+
+        $string = 'exam!ple-_@';
+        $sanitized = \TypeRocket\Utility\Sanitize::underscore($string);
+        $this->assertTrue( $sanitized == 'example__' );
     }
 }
