5c36c1c99f2abfd385c01f9e86f745cb3373bf7f
kevin dees <kevin@robojuice.com>
2016-08-30 12:41:49 -0400

    disable emailing in tests and return true

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,7 +8,7 @@ if( ! file_exists($wp_load) ) {
     echo 'PHP Unit: WordPress Not Connected > ' . $wp_load . PHP_EOL;
 } else {
     define('WP_USE_THEMES', false);
-    function wp_mail( $to, $subject, $message, $headers = '', $attachments = array() ) {}
+    function wp_mail( $to, $subject, $message, $headers = '', $attachments = array() ) { return true; }
     global $wp, $wp_query, $wp_the_query, $wp_rewrite, $wp_did_header;
     require BASE_WP . '/wp-load.php';
     require BASE_WP . '/wp-admin/includes/user.php';
