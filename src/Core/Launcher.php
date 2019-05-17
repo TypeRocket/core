e004dde433d50a63c9197155bf0e115b7c9f9e20
Kevin Dees <kvdees@gmail.com>
2019-05-17 17:00:14 -0400

    fix bug

--- src/Core/Launcher.php
+++ src/Core/Launcher.php
@@ -380,9 +380,9 @@ class Launcher
     public function addTopJs()
     {
         $scheme = '';
-        if ( ( isset( $_SERVER['HTTPS'] ) && 'on' === $_SERVER['HTTPS'] ) || ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && 'https' === $_SERVER['HTTP_X_FORWARDED_PROTO'] ) {
-	    $scheme =  'https';
-	}
+        if ( is_ssl() || ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && 'https' === $_SERVER['HTTP_X_FORWARDED_PROTO'] ) ) {
+            $scheme =  'https';
+        }
         ?><script>window.trHelpers = {site_uri: "<?php echo esc_url(home_url( '', $scheme ));?>"}</script><?php
     }
 
