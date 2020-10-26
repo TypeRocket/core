db9a2dd756e3cb054467c5dfd6831dde61162ac8
Kevin Dees <kvdees@gmail.com>
2020-10-26 16:39:08 -0400

    cleanup code

--- src/Console/Commands/CoreUpdate.php
+++ src/Console/Commands/CoreUpdate.php
@@ -26,21 +26,21 @@ class CoreUpdate extends Command
         $pro = \TypeRocket\Core\Config::get('paths.pro') . '/assets/dist';
 
         if(file_exists($core)) {
-            (new File($core))->copyTo($path);
+            File::new($core)->copyTo($path);
             $this->success('Core assets updated');
         } else {
             $this->error('Core assets not found');
         }
 
         if(file_exists($pro)) {
-            (new File($pro))->copyTo($path);
+            File::new($pro)->copyTo($path);
             $this->success('Pro assets updated');
 
             // merge manifests
             $mix = json_decode(file_get_contents($core . '/mix-manifest.json'), true);
             $mp = json_decode(file_get_contents($pro . '/mix-pro.json'), true);
             $both = array_merge($mix, $mp);
-            $all = json_encode($both, JSON_UNESCAPED_SLASHES);
+            $all = json_encode($both, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
             file_put_contents($path . '/mix-manifest.json', $all);
             $this->success('mix-manifest.json and mix-pro.json merged');
         } else {
