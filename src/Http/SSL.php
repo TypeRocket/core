d102adef190e7e4517cdaf627e6b31965dfa2273
Kevin Dees <kvdees@gmail.com>
2020-11-02 11:00:24 -0500

    fix bug

--- src/Http/SSL.php
+++ src/Http/SSL.php
@@ -3,18 +3,25 @@ namespace TypeRocket\Http;
 
 class SSL
 {
-	/**
-	 * Fix SSL URL
-	 *
-	 * @param string $url
-	 *
-	 * @return mixed
-	 */
-	public static function fixSSLUrl( $url ) {
-		$scheme = \is_ssl() ? 'https' : 'http';
-		if( $scheme == 'https' ) {
-			$url = preg_replace('/^http:/i', 'https:', $url);
-		}
+    /**
+     * Fix SSL URL
+     *
+     * @param string $url
+     * @param bool $secure_only
+     *
+     * @return mixed
+     */
+	public static function fixSSLUrl( $url, $secure_only = false )
+    {
+	    if(function_exists('is_ssl')) {
+            $scheme = $secure_only || \is_ssl() ? 'https' : 'http';
+        } elseif ($secure_only) {
+            $scheme = 'https';
+        }
+
+        if( $scheme == 'https' ) {
+            $url = preg_replace('/^http:/i', 'https:', $url);
+        }
 
 		return $url;
     }
