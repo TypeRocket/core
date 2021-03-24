0d34adcf8452707e04c9d5dc0bfcec993f6a9fb0
Kevin Dees <kevin@robojuice.com>
2021-03-24 13:08:21 -0400

    add setLabels to taxonomy class

--- src/Register/Taxonomy.php
+++ src/Register/Taxonomy.php
@@ -194,8 +194,24 @@ class Taxonomy extends Registrable
             'view_item'                  => sprintf( _x( 'View %s', $context, 'typerocket-core' ), $upperSingular),
         ];
 
-        $this->args['label'] = $upperPlural;
-        $this->args['labels'] = $labels;
+        $this->setLabels($labels, $upperPlural);
+
+        return $this;
+    }
+
+    /**
+     * Set Labels
+     *
+     * @param array $labels
+     * @param string $plural
+     * @param bool $merge
+     *
+     * @return Taxonomy $this
+     */
+    public function setLabels(array $labels, $plural = null, $merge = true)
+    {
+        $this->args['labels'] = $merge ? array_merge($this->args['labels'] ?? [], $labels) : $labels;
+        $this->args['label'] = $plural ?? $this->args['label'];
 
         return $this;
     }
