2328c17a4b56abab7d465b5206b1c526414ce2ac
Kevin Dees <kevin@robojuice.com>
2021-03-31 09:59:25 -0400

    add users and comments to QueryCaster.php

--- src/Utility/QueryCaster.php
+++ src/Utility/QueryCaster.php
@@ -2,8 +2,10 @@
 namespace TypeRocket\Utility;
 
 use TypeRocket\Database\Results;
+use TypeRocket\Models\WPComment;
 use TypeRocket\Models\WPPost;
 use TypeRocket\Models\WPTerm;
+use TypeRocket\Models\WPUser;
 
 class QueryCaster
 {
@@ -52,5 +54,51 @@ class QueryCaster
         $results = new $results_class;
 
         return $results->exchangeAndCast($wp_query->terms, get_class($model));
+    }
+
+    /**
+     * @param string $model must be a class of WPUser
+     * @param array $wp_query_args args for WP_User_Query
+     *
+     * @return Results
+     * @throws \Exception
+     */
+    public static function users(string $model, array $wp_query_args)
+    {
+        $model = new $model;
+
+        if(!$model instanceof WPUser) {
+            throw new \Exception(static::class . ' requires a WPUser model. You provided: ' . get_class($model));
+        }
+
+        $wp_query = new \WP_User_Query($wp_query_args);
+        $results_class = $model->getResultsClass();
+        /** @var Results $results */
+        $results = new $results_class;
+
+        return $results->exchangeAndCast($wp_query->get_results(), get_class($model));
+    }
+
+    /**
+     * @param string $model must be a class of WPComment
+     * @param array $wp_query_args args for WP_Comment_Query
+     *
+     * @return Results
+     * @throws \Exception
+     */
+    public static function comments(string $model, array $wp_query_args)
+    {
+        $model = new $model;
+
+        if(!$model instanceof WPComment) {
+            throw new \Exception(static::class . ' requires a WPComment model. You provided: ' . get_class($model));
+        }
+
+        $wp_query = new \WP_Comment_Query($wp_query_args);
+        $results_class = $model->getResultsClass();
+        /** @var Results $results */
+        $results = new $results_class;
+
+        return $results->exchangeAndCast($wp_query->comments, get_class($model));
     }
 }
