d754f04b06555fd6ea3931ae28e456bd6c2f0574
Kevin Dees <kevin@robojuice.com>
2021-05-18 16:30:57 -0400

    add with_front rewrite option to PostType class

--- src/Register/PostType.php
+++ src/Register/PostType.php
@@ -621,9 +621,29 @@ class PostType extends Registrable
      *
      * @return PostType $this
      */
-    public function setSlug( $slug )
+    public function setSlug( $slug, $withFront = null )
     {
-        $this->args['rewrite'] = ['slug' => Sanitize::dash( $slug )];
+        if(!is_array($this->args['rewrite'])) {
+            $this->args['rewrite'] = [];
+        }
+
+        $this->args['rewrite']['slug'] = Sanitize::dash( $slug );
+
+        if(isset($withFront)) {
+            $this->args['rewrite']['with_front'] = $withFront;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Disable Slug With Front
+     *
+     * @return $this
+     */
+    public function disableSlugWithFront()
+    {
+        $this->args['rewrite']['with_front'] = false;
 
         return $this;
     }
