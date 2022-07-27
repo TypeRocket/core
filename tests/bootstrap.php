899237fd0a184164f1284cbb95534fecab82df14
Kevin Dees <kvdees@gmail.com>
2022-07-27 11:18:11 -0400

    bump PHP version requirement to ^7.4 and typerocket/core to ^5.1 move woocommerce/action-scheduler into typerocket/core bump phpunit to ^9.5

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,10 @@
 <?php
 require __DIR__.'/../vendor/autoload.php';
+
+if(file_exists(__DIR__.'/../../../vendor/autoload.php')) {
+    require_once __DIR__.'/../vendor/autoload.php';
+}
+
 date_default_timezone_set('UTC');
 
 $_SERVER['SERVER_PROTOCOL'] = $_SERVER['SERVER_PROTOCOL'] ?? null;
