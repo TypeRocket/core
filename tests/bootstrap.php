9c972326d0ff9b664b54cb1ab9d4254b61c74ba5
Kevin Dees <kvdees@gmail.com>
2022-07-27 12:41:07 -0400

    improve phpunit loading via composer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
-require __DIR__.'/../vendor/autoload.php';
+if(file_exists(__DIR__.'/../vendor/autoload.php')) {
+    require_once __DIR__.'/../vendor/autoload.php';
+}
 
 if(file_exists(__DIR__.'/../../../vendor/autoload.php')) {
     require_once __DIR__.'/../vendor/autoload.php';
