97cc377600cc40e850407545d9be381f2213677f
Kevin Dees <kvdees@gmail.com>
2022-08-25 22:25:51 -0400

    add createMapIndexBy to data class

--- src/Utility/Data.php
+++ src/Utility/Data.php
@@ -81,6 +81,37 @@ class Data
     }
 
     /**
+     * Index Data by Unique Value
+     *
+     * @param string $index
+     * @param array<int, object|array> $array an array to traverse with object or arrays
+     *
+     * @return array<int, object|array>
+     * @throws \Exception
+     */
+    public static function createMapIndexBy(string $index, array $array) : array
+    {
+        $indexed_list = [];
+
+        foreach ($array as $item) {
+            if(!is_array($item) || !is_object($item)) {
+                $type = gettype($item);
+                throw new \Exception("Nested array or object required for Data::createMapIndexBy(): {$type} is not valid.");
+            }
+
+            $key = is_object($array) ? ($array->$index ?? null) : ($array[$index] ?? null);
+
+            if(array_key_exists($key, $indexed_list)) {
+                throw new \Exception("Array key must be unique for Data::createMapIndexBy(): {$key} already taken.");
+            }
+
+            $indexed_list[$key] = $item;
+        }
+
+        return $indexed_list;
+    }
+
+    /**
      * @param mixed $value
      * @param string|callable $type
      *
