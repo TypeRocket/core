3e1e2f85d459927a7224e681d012ec6eb973dec8
Kevin Dees <kvdees@gmail.com>
2022-09-02 10:58:01 -0400

    update ArrTest

--- tests/Utility/ArrTest.php
+++ tests/Utility/ArrTest.php
@@ -1,8 +1,13 @@
 <?php
 namespace Utility;
 
+use JetBrains\PhpStorm\Internal\LanguageLevelTypeAware;
+use JetBrains\PhpStorm\Internal\TentativeType;
 use PHPUnit\Framework\TestCase;
 use TypeRocket\Utility\Arr;
+use TypeRocket\Utility\Traits\ArrayAccessible;
+use TypeRocket\Utility\Traits\ArrayIterable;
+use YoastSEO_Vendor\League\OAuth2\Client\Tool\ArrayAccessorTrait;
 
 class ArrTest extends TestCase
 {
@@ -199,6 +204,158 @@ class ArrTest extends TestCase
         $this->assertTrue(!Arr::has($data, ['one.two', 'one', '']));
     }
 
+    public function testArrayHasManyArrayObject()
+    {
+        $class = new \ArrayObject();
+        $class['one'] = ['two' => null];
+
+        $this->assertTrue(Arr::has($class, ['one.two', 'one']));
+        $this->assertTrue(!Arr::has($class, ['one.two', 'one', 'three']));
+        $this->assertTrue(!Arr::has($class, ['one.two', 'one', '']));
+    }
+
+    public function testArrayHasManyArrayAccess()
+    {
+        $class = new class implements \ArrayAccess, \Iterator {
+
+            use ArrayAccessible, ArrayIterable;
+
+            protected array $storage = [];
+            protected string $_location = 'storage';
+
+            public function __construct() {
+                $this->storage = [
+                    "one"   => ['two' => null],
+                    "99"   => 99,
+                ];
+            }
+        };
+
+        $this->assertTrue(Arr::has($class, ['one.two', 'one']));
+        $this->assertTrue(!Arr::has($class, ['one.two', 'one', 'three']));
+        $this->assertTrue(!Arr::has($class, ['one.two', 'one', '']));
+    }
+
+    public function testArrayHasManyArrayAccessIterator()
+    {
+        $class = new class implements \Iterator, \ArrayAccess {
+
+            use ArrayAccessible, ArrayIterable;
+
+            protected array $storage = [];
+            protected string $_location = 'storage';
+
+            public function __construct() {
+                $this->storage = [
+                    "one"   => ['two' => null],
+                    "two"   => ['two' => 0],
+                    "three"   => 0,
+                ];
+            }
+        };
+
+        $class['four'] = null;
+
+        $this->assertTrue($class['three'] === 0);
+        $this->assertTrue($class['four'] === null);
+
+        // run once
+        foreach ($class as $key => $value) {
+            if(in_array($key, ['one', 'two'])) {
+                $this->assertTrue(Arr::has($value, 'two'));
+                $this->assertTrue(Arr::has($value, ['two']));
+                $this->assertTrue( ! Arr::has($value, ['ten']));
+                $this->assertTrue( ! Arr::has($value, ['ten']));
+            } elseif( $key === 'three' ) {
+                $this->assertTrue($value === 0);
+            } else {
+                $this->assertTrue($value === null);
+            }
+        }
+
+        // run twice
+        foreach ($class as $key => $value) {
+            if(in_array($key, ['one', 'two'])) {
+                $this->assertTrue(Arr::has($value, 'two'));
+                $this->assertTrue(Arr::has($value, ['two']));
+                $this->assertTrue( ! Arr::has($value, ['ten']));
+                $this->assertTrue( ! Arr::has($value, ['ten']));
+            } elseif( $key === 'three' ) {
+                $this->assertTrue($value === 0);
+            } else {
+                $this->assertTrue($value === null);
+            }
+        }
+
+        $this->assertTrue(Arr::has($class, ['one', 'two']));
+    }
+
+    public function testArrayHasManyArrayIteratorAssoc()
+    {
+        $class = new class implements \Iterator {
+
+            use ArrayIterable;
+
+            protected array $storage = [];
+            protected string $_location = 'storage';
+
+            public function __construct() {
+                $this->storage = [
+                    "one"   => ['two' => null],
+                    "two"   => ['two' => 0],
+                    "three"   => 0,
+                ];
+            }
+        };
+
+        // run once
+        foreach ($class as $key => $value) {
+            if(in_array($key, ['one', 'two'])) {
+                $this->assertTrue(Arr::has($value, 'two'));
+                $this->assertTrue(Arr::has($value, ['two']));
+                $this->assertTrue( ! Arr::has($value, ['ten']));
+                $this->assertTrue( ! Arr::has($value, ['ten']));
+            } elseif( $key === 'three' ) {
+                $this->assertTrue($value === 0);
+            } else {
+                $this->assertTrue($value === null);
+            }
+        }
+    }
+
+    public function testArrayHasManyArrayIteratorSequential()
+    {
+        $class = new class implements \Iterator {
+
+            use ArrayIterable;
+
+            protected array $storage = [];
+            protected string $_location = 'storage';
+
+            public function __construct() {
+                $this->storage = [
+                    ['two' => null],
+                    ['two' => 0],
+                    0,
+                ];
+            }
+        };
+
+        // run once
+        foreach ($class as $key => $value) {
+            if(in_array($key, [0,1])) {
+                $this->assertTrue(Arr::has($value, 'two'));
+                $this->assertTrue(Arr::has($value, ['two']));
+                $this->assertTrue( ! Arr::has($value, ['ten']));
+                $this->assertTrue( ! Arr::has($value, ['ten']));
+            } elseif( $key === 2 ) {
+                $this->assertTrue($value === 0);
+            } else {
+                $this->assertTrue($value === null);
+            }
+        }
+    }
+
     public function testArrayHas()
     {
         $data = ['one' => ['two' => null]];
@@ -206,6 +363,76 @@ class ArrTest extends TestCase
         $this->assertTrue(Arr::has($data, 'one'));
         $this->assertTrue(!Arr::has($data, 'one.two.three'));
         $this->assertTrue(!Arr::has($data, ''));
+    }
+
+    public function testArrayFirstLast()
+    {
+        $array = new \ArrayObject();
+        $array['one'] = 1;
+        $array['two'] = 2;
+        $array['three'] = 3;
+
+        $this->assertTrue(Arr::first($array) === 1);
+        $this->assertTrue(Arr::last($array) === 3);
+        $this->assertTrue(Arr::last([]) === null);
+        $this->assertTrue(Arr::first([]) === null);
+
+        $this->assertTrue(Arr::first($array, function($v, $k) {
+            return $k === 'two';
+        }) === 2);
+
+        $this->assertTrue(Arr::first($array, function($v, $k) {
+            return $v === 3;
+        }) === 3);
+
+        $this->assertTrue(Arr::last($array, function($v, $k) {
+                return $v === 10 || $k === 'ten';
+        }, 4) === 4);
+    }
+
+    public function testArrayExists()
+    {
+        $array = [
+            'one' => 3,
+            1.4 => 3,
+            '1.1' => 3,
+            1 => 3,
+        ];
+
+        $this->assertTrue(Arr::exists($array, 'one'));
+        $this->assertTrue(Arr::exists($array, 1));
+        $this->assertTrue(Arr::exists($array, 1.1));
+        $this->assertTrue( ! Arr::exists($array, 1.4));
+        $this->assertTrue( ! Arr::exists($array, 0));
+    }
+
+    public function testArrayExistsObject()
+    {
+        $array = new \ArrayObject();
+        $array['one'] = 3;
+        $array[1.4] = 3;
+        $array['1.1'] = 3;
+        $array[1] = 3;
+
+        $this->assertTrue(Arr::exists($array, 'one'));
+        $this->assertTrue(Arr::exists($array, 1));
+        $this->assertTrue(Arr::exists($array, 1.1));
+        $this->assertTrue( ! Arr::exists($array, 1.4));
+        $this->assertTrue( ! Arr::exists($array, 0));
+    }
+
+    public function testArrayFirstLastObject()
+    {
+        $array = [
+            'one' => 1,
+            'two' => 2,
+            'three' => 3,
+        ];
+
+        $this->assertTrue(Arr::first($array) === 1);
+        $this->assertTrue(Arr::last($array) === 3);
+        $this->assertTrue(Arr::last([]) === null);
+        $this->assertTrue(Arr::first([]) === null);
     }
 
     public function testArraySet()
